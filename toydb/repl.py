4ffa96024f031aa6dc4b6cc8b621ce38a0e70cec
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2020-04-11 11:06:37 +0200

    Add pretty printing

--- toydb/repl.py
+++ toydb/repl.py
@@ -1,6 +1,6 @@
 import sys
 from enum import Enum
-from typing import List, Optional, Sequence, Tuple, Type
+from typing import List, Optional, Sequence, Tuple, Type, Union
 
 from blessed import Terminal
 from prompt_toolkit import PromptSession
@@ -67,6 +67,40 @@ def parse_command(command: str) -> Optional[Command]:
     return None
 
 
+def copy_nested_list(lst: List[List[str]]) -> List[List[str]]:
+    """Return a copy of list l to one level of nesting"""
+    return [list(i) for i in lst]
+
+
+def column_widths(table: List[List[str]]) -> List[int]:
+    """Get the maximum size for each column in table"""
+    return [max(map(len, col)) for col in zip(*table)]
+
+
+def align_table(table: List[List[str]]) -> List[List[str]]:
+    """Return table justified according to align"""
+    widths = column_widths(table)
+    new_table = copy_nested_list(table)
+    align = "<"
+    for row in new_table:
+        for cell_num, cell in enumerate(row):
+            row[cell_num] = "{:{align}{width}}".format(cell, align=align, width=widths[cell_num])
+    return new_table
+
+
+def stringify_table(table: List[List[Union[int, str]]]) -> List[List[str]]:
+    new_table = []
+    for row in table:
+        new_row = [str(i) for i in row]
+        new_table.append(new_row)
+    return new_table
+
+
+def print_selection(table: List[List[Union[int, str]]]) -> None:
+    for row in align_table(stringify_table(table)):
+        print(" ".join(row))
+
+
 def handle_command(table: Optional[Table], command: Command) -> Optional[Table]:
     if isinstance(command, CreateTable):
         table = Table(name=command.table_name, columns=command.columns)
@@ -85,8 +119,8 @@ def handle_command(table: Optional[Table], command: Command) -> Optional[Table]:
             if table is None:
                 print("no table selected")
             else:
-                for record in table.select(command.columns):
-                    print(record)
+                table_ = list(table.select(command.columns))
+                print_selection(table_)
         else:
             raise ValueError("command not handled")
     return table
